e789aff4c422bcaf5f7983788a3546d33e8e58d5
Qw3ko <bannikov.arseniyy@rambler.ru>
2025-02-19 21:41:36 +0500

    minor fix

--- pages/lecture/[id].tsx
+++ pages/lecture/[id].tsx
@@ -7,7 +7,11 @@ const LecturePage: FC = () => {
 	const { id } = router.query
 
 	if (!router.isReady) {
-		return <div>Подготовка маршрута...</div>
+		return (
+			<div className='container h-[700px] flex justify-center items-center mx-auto my-12 p-4 font-azoft uppercase font-bold text-[48px]'>
+				Загрузка данных...
+			</div>
+		)
 	}
 
 	return <LectureCard id={id as string} />
